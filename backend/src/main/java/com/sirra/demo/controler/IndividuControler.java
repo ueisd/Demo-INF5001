0f56129d77d26310ff5a3e098601012eec8e266a
Pierre-Luc Maître <pierrelucueisd@gmail.com>
2019-11-22 19:08:08 -0500

    changmeent de croos origin pour localhost

--- backend/src/main/java/com/sirra/demo/controler/IndividuControler.java
+++ backend/src/main/java/com/sirra/demo/controler/IndividuControler.java
@@ -18,7 +18,7 @@ import java.util.List;
 
 @Api(description = "Gestion des Individus")
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost")
 public class IndividuControler {
 
     @Autowired
