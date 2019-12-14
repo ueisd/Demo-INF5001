10e8ea61b537ffd9ba0cf924ea5e0700215c97c2
Pierre-Luc Maître <pierrelucueisd@gmail.com>
2019-12-14 15:40:19 -0500

    Ajout d'une classe pour effecturer des tests unitaires.

---
+++ backend/src/test/java/com/sirra/demo/model/LigneDeTempsTest.java
@@ -0,0 +1,16 @@
+package com.sirra.demo.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LigneDeTempsTest {
+
+    @Test
+    void retirerMinutesFin() {
+    }
+
+    @Test
+    void decalerADroiteDeSecondes() {
+    }
+}
